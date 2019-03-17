05e9543cf37588920a999e10d77e903f59fca961
Andrew Beers <apbeers@gmail.com>
2019-03-17 16:56:22 -0500

    test picking either model

--- highest_of_either_model.py
+++ highest_of_either_model.py
@@ -11,9 +11,8 @@ good_queries_tld = list(set(open('goodqueries_tld.txt', 'r', encoding='utf-8')))
 bad_queries_full_url = list(set(open('badqueries_full_url.txt', 'r', encoding='utf-8')))
 good_queries_full_url = list(set(open('goodqueries_full_url.txt', 'r', encoding='utf-8')))
 
-# Will take 2% of each dataset to use for testing
 
-test_size = 20
+test_percent = .2
 
 print('Shuffling')
 np.random.shuffle(bad_queries_tld)
@@ -21,12 +20,14 @@ np.random.shuffle(good_queries_tld)
 np.random.shuffle(bad_queries_full_url)
 np.random.shuffle(good_queries_full_url)
 
-print('Splitting')
-bad_queries_tld_train, bad_queries_tld_test = bad_queries_tld[test_size:], bad_queries_tld[:test_size]
-good_queries_tld_train, good_queries_tld_test = good_queries_tld[test_size:], bad_queries_tld[:test_size]
 
-bad_queries_full_url_train, bad_queries_full_url_test = bad_queries_full_url[test_size:], bad_queries_full_url[:test_size]
-good_queries_full_url_train, good_queries_full_url_test = good_queries_full_url[test_size:], good_queries_full_url[:test_size]
+
+print('Splitting')
+bad_queries_tld_train, bad_queries_tld_test = bad_queries_tld[int(len(bad_queries_tld) * (1-test_percent)):], bad_queries_tld[:int(len(bad_queries_tld) - (len(bad_queries_tld) * test_percent))]
+good_queries_tld_train, good_queries_tld_test = good_queries_tld[int(len(good_queries_tld) * (1-test_percent)):], bad_queries_tld[:int(len(good_queries_tld) - (len(good_queries_tld) * test_percent))]
+
+bad_queries_full_url_train, bad_queries_full_url_test = bad_queries_full_url[int(len(bad_queries_full_url) * (1-test_percent)):], bad_queries_full_url[:int(len(bad_queries_full_url) - (len(bad_queries_full_url) * test_percent))]
+good_queries_full_url_train, good_queries_full_url_test = good_queries_full_url[int(len(good_queries_full_url) * (1-test_percent)):], good_queries_full_url[:int(len(good_queries_full_url) - (len(good_queries_full_url) * test_percent))]
 
 print('Assigning Categories')
 tld_all_train = bad_queries_tld_train + good_queries_tld_train
@@ -54,7 +55,11 @@ full_url_lgs = LogisticRegression(class_weight={1: 2 * len(bad_queries_full_url_
 full_url_lgs.fit(full_url_vector, full_url_y)
 
 print('Validating')
-for query in bad_queries_tld_test:
+correct = 0
+incorrect = 0
+tld_model = 0
+full_url_model = 0
+for query in bad_queries_tld:
     tld_probability = tld_lgs.predict_proba(tld_vectorizer.transform([query]))
     tld_percent_true = float(tld_probability[0][0])
     tld_percent_false = float(tld_probability[0][1])
@@ -73,10 +78,24 @@ for query in bad_queries_tld_test:
 
     if tld_accuracy > full_url_accuracy:
         prediction = tld_lgs.predict(tld_vectorizer.transform([query]))
+        tld_model += 1
     else:
         prediction = full_url_lgs.predict(full_url_vectorizer.transform([query]))
+        full_url_model += 1
 
     if prediction:
-        print('correct')
+        correct += 1
     else:
-        print(query)
+        incorrect += 1
+
+
+print('correct: {0}'.format(correct))
+print('incorrect: {0}'.format(incorrect))
+print('full url model: {0}'.format(full_url_model))
+print('tld model: {0}'.format(tld_model))
+print('accuracy: {0}'.format(correct/(correct + incorrect)))
+print('num samples: {0}'.format(correct + incorrect))
+
+print('{0}'.format(correct + incorrect), end=',')
+print('{0}'.format(correct/(correct + incorrect)), end=',')
+print('{0}'.format(full_url_model/(full_url_model + tld_model)), end='')
